65c6e91c48ea97989ca241ff86192fe9dcf8d29c
Airat <omg.airat@gmail.com>
2021-10-12 19:06:50 +0300

    mobile

--- js/rem.js
+++ js/rem.js
@@ -1,13 +1,28 @@
-function onRem () {
-  let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  let iw = (iOS) ? screen.width : window.innerWidth, ih = (iOS) ? screen.height : window.innerHeight;
+// function onRem () {
+//   let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+//   let iw = (iOS) ? screen.width : window.innerWidth, ih = (iOS) ? screen.height : window.innerHeight;
+//
+//   switch (true) {
+//     case (iw > 650):
+//       document.documentElement.style.fontSize = iw / 1920 + 'px';
+//       console.log(iw)
+//       break;
+//     case (iw <= 650):
+//       document.documentElement.style.fontSize = iw / 320 + 'px';
+//       break;
+//   }
+// }
 
+
+function onRem() {
+  let width = document.documentElement.clientWidth;
   switch (true) {
-    case (iw > 650):
-      document.documentElement.style.fontSize = iw / 1920 + 'px';
+    case (width > 650):
+      document.documentElement.style.fontSize = width / 1920 + 'px';
+      console.log(width);
       break;
-    case (iw <= 650):
-      document.documentElement.style.fontSize = iw / 320 + 'px';
+    case (width <= 650):
+      document.documentElement.style.fontSize = width / 320 + 'px';
       break;
   }
 }
